9ab5a3962d35b92a1102369a207c9044504fafc4
ZhouYu <zhouyuwinner@163.com>
2018-11-04 20:16:27 +0800

    computer

---
+++ 2-高级-包管理/03-from mport.py
@@ -0,0 +1,15 @@
+# from p01 import Student, sayHello
+#
+# stu = Student()
+#
+# stu.say()
+#
+# sayHello()
+
+from p01 import *
+
+sayHello()
+
+stu = Student("qiongqiong", 28)
+
+stu.say()
---
+++ 2-高级-包管理/pkg02/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['p01']
+
+def __inInit__():
+    print("I an in init of package")
