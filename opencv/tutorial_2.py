9cdff1f59d0a05ebe33273e62187028db673b2a0
ZhouYu <zhouyuwinner@163.com>
2018-11-08 20:46:52 +0800

    computer

--- opencv/tutorial_2.py
+++ opencv/tutorial_2.py
@@ -15,6 +15,12 @@ def access_pixels(image):
                 image[row, col, c] = 255 - pv
     cv.imshow("pixel_date", image)
 
+# 像素去反,inverse是自带的API，速度快
+def inverse(image):
+    dst = cv.bitwise_not(image)
+    cv.imshow("取反", dst)
+
+
 def creat_image():
     '''
     三通道
@@ -29,18 +35,26 @@ def creat_image():
     cv.imwrite("C:\\Users\\ZhouYu\\Desktop\\black.png", img)
     '''
 
-    m1 = np.ones([3, 3], np.float32)  float32是浮点数，uint8是整数，int32
-    m1.fill(122.388)
+    m1 = np.ones([3, 3], np.uint8)  # float32是浮点数，uint8是整数，int32
+    # m1.fill(122.388)
     print(m1)
+
+    m2 = m1.reshape([1, 9])  # 二维转一维
+    print(m2)
+
+    m3 = np.array([[2, 1, 2], [3, 1, 5], [4, 5, 1]], np.int32)  # 数值任意赋予
+    m3.fill(9)
+    print(m3)
 
 src = cv.imread("C:\\Users\\ZhouYu\\Desktop\\1.jpg")  # blue, green, red
 cv.namedWindow("input image", cv.WINDOW_AUTOSIZE)
 cv.imshow("input image", src)
 t1 = cv.getTickCount()
+# inverse(src)
 # access_pixels(src)
 creat_image()
 t2 = cv.getTickCount()
-print("time: {0} s".format((t2-t1)/cv.getTickFrequency()))
+print("time: {0} s".format((t2-t1)/cv.getTickFrequency()))  # 计算运算时间
 cv.waitKey(0)
 
 cv.destroyAllWindows()
---
+++ opencv/tutorial_4.py
@@ -0,0 +1,64 @@
+import cv2 as cv
+import numpy as np
+
+
+def add_demo(m1, m2):
+    dst = cv.add(m1, m2)
+    cv.imshow("add_demo", dst)
+
+
+def subtract_demo(m1, m2):
+    dst = cv.subtract(m1, m2)
+    cv.imshow("subtract_demo", dst)
+
+
+def divide_demo(m1, m2):
+    dst = cv.divide(m1, m2)
+    cv.imshow("divide_demo", dst)
+
+
+def multiply_demo(m1, m2):
+    dst = cv.multiply(m1, m2)
+    cv.imshow("multiply_demo", dst)
+
+
+def mean_demo(m1, m2):
+    M1 = cv.mean(m1)
+    M2 = cv.mean(m2)
+    print(M1)
+    print(M2)
+
+
+def mean_dev_demo(m1, m2):
+    M1, dev1 = cv.meanStdDev(m1)
+    M2, dev2 = cv.meanStdDev(m2)
+    h, w = m1.shape[0:2]  # 获取数组m1[高，宽，通道数],
+    print(M1)
+    print(M2)
+    print(dev1)
+    print(dev2)
+
+    img = np.zeros([h, w], np.uint8)
+    m, dev = cv.meanStdDev(img)
+    print(m)
+    print(dev)
+
+
+src1 = cv.imread("C:\\Users\\ZhouYu\\Desktop\\8.jpg")
+src2 = cv.imread("C:\\Users\\ZhouYu\\Desktop\\9.jpg")
+print(src1.shape)
+print(src2.shape)
+cv.namedWindow("input image1", cv.WINDOW_AUTOSIZE)
+cv.imshow("input image1", src1)
+cv.imshow("input image2", src2)
+
+
+# add_demo(src1, src2)
+# subtract_demo(src1, src2)
+# divide_demo(src1, src2)
+# multiply_demo(src1, src2)
+mean_demo(src1, src2)
+mean_dev_demo(src1, src2)
+cv.waitKey(0)
+
+cv.destroyAllWindows()
